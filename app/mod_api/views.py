b24e8c43908abbfb36ca072320f919e0a551d0b8
gramos <you@example.com>
2015-03-17 19:58:12 +0100

    adding the aggregate query

--- app/mod_api/views.py
+++ app/mod_api/views.py
@@ -2,6 +2,7 @@ from flask import Blueprint, render_template
 from flask.views import View
 from flask import Response
 from bson import json_util
+from bson.son import SON
 
 from app import mongo
 
@@ -14,73 +15,45 @@ def route(route_slug):
 	Hardcoded for now, but this should be a MongoDB query
 	that fetches the document where { 'slug': route_slug }
 	'''
-	route_merc_kalt = {
-		'name': 'Merc.Kalt.03 802 AM',
-		'slug': 'merc-kalt-03-802-am',
-		'coordinates':[
-			{
-				'lng': 20.4265936,
-				'lat': 42.388864,
-				'weight': 100
-			},
-			{
-				'lng': 20.4266192,
-				'lat': 42.3888352,
-				'weight': 1
-			},
-			{
-				'lng': 20.4267232,
-				'lat': 42.3888256,
-				'weight': 1
-			},
-			{
-				'lng': 20.426744,
-				'lat': 42.3888288,
-				'weight': 1
-			},
-			{
-				'lng': 20.4267904,
-				'lat': 42.3888576,
-				'weight': 2
-			},
-		]
-	}
 
-	route_merc_cist = {
-		'name': 'Merc.Cist.03631BC',
-		'slug': 'merc-cist-03631bc',
-		'coordinates':[
-			{
-				'lng': 20.4265136,
-				'lat': 42.3889248,
-				'weight': 53
-			},
-			{
-				'lng': 20.426496,
-				'lat': 42.3887168,
-				'weight': 1
-			},
-			{
-				'lng': 20.4265392,
-				'lat': 42.3887776,
-				'weight': 1
-			},
-			{
-				'lng': 20.4265904,
-				'lat': 42.3888576,
-				'weight': 68
+	docs = mongo.db.wasteroutes.aggregate([
+		{
+			"$match":{
+				"route.slug": route_slug
 			}
-		]
-	}
+		},
+		{
+			"$group":{
+				"_id":{
+				 	"rank":"$rank",
+				 	"lat": "$lat",
+				 	"lng": "$lng"
+				},
+				"count":{
+					"$sum":1
+				}
+			}
+		},
+		{
+			"$sort": {
+				"_id.rank": 1
+			}	
+		},
+		{
+			"$project":{
+				"_id": 0,
+				"lat":"$_id.lat",
+				"lng":"$_id.lng",
+				"count":"$count"
+			}	
+		}
+	])
 
-	route_coordinates = []
-	if route_slug == 'merc-kalt-03-802-am':
-		route_coordinates = route_merc_kalt['coordinates']
-	elif route_slug == 'merc-cist-03631bc':
-		route_coordinates = route_merc_cist['coordinates']
-    
+	result = docs['result']
+
+
 	resp = Response(
-            response=json_util.dumps(route_coordinates),
+            response=json_util.dumps(result),
             mimetype='application/json')
 
 	return resp
