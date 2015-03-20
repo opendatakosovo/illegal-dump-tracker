d889270186e1807ef225cff6df0c1220b7dd4f11
endrit-b <endriti.92@gmail.com>
2015-03-20 01:51:37 +0100

    analytics based on type and size of illegal dumps

---
+++ app/static/data/aggregations.js
@@ -0,0 +1,18 @@
+var aggregation = {
+	"types":[
+		{ "count" : 37, "type" : "AUTOMOTIVE" },
+		{ "count" : 200, "type" : "CONSTRUCTION" },
+		{ "count" : 21, "type" : "ELECTRONIC" },
+		{ "count" : 62, "type" : "LIQUID" },
+		{ "count" : 220, "type" : "GLASS" },
+		{ "count" : 29, "type" : "DANGEROUS" },
+		{ "count" : 420, "type" : "PLASTIC" },
+		{ "count" : 147, "type" : "METAL" },
+		{ "count" : 271, "type" : "HOUSEHOLD" },
+	],
+	"sizes":[
+		{ "count" : 156, "type" : "MEDIUM" },
+		{ "count" : 137, "type" : "LARGE" },
+		{ "count" : 277, "type" : "SMALL" },
+	]
+}
