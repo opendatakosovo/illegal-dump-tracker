702b1b3ef34cfca9bcce3a11d67024ce8c73b060
Georges Labreche <georges@opendatakosovo.org>
2015-03-19 19:46:50 -0700

    fixed data

--- app/static/data/aggregations.js
+++ app/static/data/aggregations.js
@@ -1,26 +1,20 @@
 var aggregation = {
 	"types":[
-		{ "count" : 7, "type" : " DANGEROUS" },
-		{ "count" : 3, "type" : " PLASTIC" },
-		{ "count" : 3, "type" : "ORGANIC" },
-		{ "count" : 14, "type" : "PLASTIC " },
-		{ "count" : 29, "type" : "DANGEROUS" },
-		{ "count" : 148, "type" : "METAL" },
-		{ "count" : 231, "type" : "CONSTRUCTION" },
-		{ "count" : 62, "type" : "LIQUID" },
-		{ "count" : 5, "type" : " GLASS" },
-		{ "count" : 230, "type" : "GLASS" },
-		{ "count" : 21, "type" : "ELECTRONIC" },
-		{ "count" : 4, "type" : " GLASS " },
 		{ "count" : 38, "type" : "AUTOMOTIVE" },
-		{ "count" : 492, "type" : "PLASTIC" },
-		{ "count" : 7, "type" : " CONSTRUCTION" },
+		{ "count" : 238, "type" : "CONSTRUCTION" },
+		{ "count" : 36, "type" : "DANGEROUS" },
+		{ "count" : 21, "type" : "ELECTRONIC" },
+		{ "count" : 239, "type" : "GLASS" },
 		{ "count" : 271, "type" : "HOUSEHOLD" },
+		{ "count" : 62, "type" : "LIQUID" },
+		{ "count" : 148, "type" : "METAL" },
+		{ "count" : 3, "type" : "ORGANIC" },
+		{ "count" : 509, "type" : "PLASTIC" }	
 	],
-	"sizes":[
-		{ "count" : 178, "type" : "LARGE" },
-		{ "count" : 208, "type" : "MEDIUM" },
+	"sizes":[	
 		{ "count" : 300, "type" : "SMALL" },
+		{ "count" : 208, "type" : "MEDIUM" },
+		{ "count" : 178, "type" : "LARGE" }
 	]
 }
 
